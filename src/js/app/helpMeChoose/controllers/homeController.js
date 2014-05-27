a03ac9004bf7d462e67e06ccccfb7dabd97a607d
Steven John Miller <steven.jmiller@gmail.com>
2014-05-27 10:07:32 +1000

    Loading + proper list

--- src/js/app/helpMeChoose/controllers/homeController.js
+++ src/js/app/helpMeChoose/controllers/homeController.js
@@ -4,7 +4,7 @@ function HomeController($scope, $timeout, $http) {
 
   $http.get('/api/stations',{})
     .success(function(data, status) {
-      $scope.stations = data;
+      $scope.stations = data.stations;
     })
     .error(function(data, status) {
       $scope.stations = data || "Request failed";
